afe248ec3278b82d2aadd5929e44281393e2ecd5
vinnyaa <stokar2014@gmail.com>
2024-10-12 06:23:47 -0500

    first assignment 7.2 attempt

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,33 @@
+
+##############################################################
+#
+# LDD make file
+#
+##############################################################
+
+
+LDD_VERSION = '806b837ec6fd864cbd7b0886b707eb1ab264f38a'
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-vinnyaa.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = scull misc-modules
+LDD_MODULE_MAKE_OPTS = KERNELDIR=$(LINUX_DIR)
+#LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+
+$(eval $(kernel-module))
+
+# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+	
+	
+endef
+
+
+$(eval $(generic-package))
