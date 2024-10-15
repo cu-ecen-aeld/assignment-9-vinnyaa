9079d92d42923da5ba811ab8b9c78cd785751397
vinnyaa <stokar2014@gmail.com>
2024-10-14 23:27:54 -0500

    finally running correctly on local

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,12 +6,12 @@
 ##############################################################
 
 
-LDD_VERSION = '11edecf7996a0a362caf49262c3ef1fd67276219'
+AESDCHAR_VERSION = '3713b34cae0234a0c32576b3d9fb17cfbadb17b0'
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-LDD_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-vinnyaa.git'
-LDD_SITE_METHOD = git
-LDD_GIT_SUBMODULES = YES
+AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-vinnyaa.git'
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
 
 
 $(eval $(kernel-module))
@@ -19,13 +19,11 @@ $(eval $(kernel-module))
 AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 AESDCHAR_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
-define AESD_INSTALL_TARGET_CMDS
+
+define AESDCHAR_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar-start-stop $(TARGET_DIR)/etc/init.d/S97aesdchar
-	
+	#$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar-start-stop $(TARGET_DIR)/usr/bin/
 endef
-
 
 $(eval $(generic-package))
